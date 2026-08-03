a164d75b99c080fc1a12b8f7b0625e01ef664457
zainh332 <zain_h332@hotmail.com>
2026-08-04 02:43:48 +0500

    fixed

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+
+const isServer = fs.existsSync('/home/corexger/public_html');
+const outDir = isServer ? '../public_html' : 'public';
 
 export default defineConfig({
   plugins: [react()],
@@ -13,7 +17,8 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'public',
+    outDir,
+    emptyOutDir: isServer ? false : true,
     target: 'es2020',
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
